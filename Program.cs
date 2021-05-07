3cecc55070b5ee1938fafd7c60d678969303c92d
barabantoto <hampik2004@gmail.com>
2021-05-07 16:59:41 +0300

    NumberFinder

--- Program.cs
+++ Program.cs
@@ -6,30 +6,31 @@ namespace Поиск_числа
     {
         static void Main()
         {
-            Console.Write("Число до которого мы ищем(оно должно бить больше 0): ");
+            Console.Write("The number we are looking for (it must be greater than 0): ");
             int until = int.Parse(Console.ReadLine());
             if (until > 0)
-                Console.WriteLine("Ваше число: " + FindNumber(0, until, ((int)until / 2)));
-            else Console.WriteLine("Мы так не играем :(((((((");
+                Console.WriteLine("Yeeees your number is: " + FindNumber(0, until, ((int)until / 2)));
+
+            else
+                Console.WriteLine("It was offensive :(((((((");
+
+            Console.ReadKey();
         }
 
         static int FindNumber(int from, int until, int halfNum)
         {
             if (from == until)
             {
-                Console.Write("Это число " + until + "?" + "(напишите пожалуйста + или - ): ");
+                Console.Write("Your number is " + until + "?" + "(write please + or - ): ");
                 string yesOrNo = Console.ReadLine();
-                if (yesOrNo.Length > 0)
-                    if (yesOrNo[0] == '+')
+                if (yesOrNo.Length > 0 && yesOrNo[0] == '+')
                         return until;
-                    else
-                        halfNum = FindNumber(from, until, halfNum);
                 else
                     halfNum = FindNumber(from, until, halfNum);
             }
             else
             {
-                Console.Write("Это число меньше или равно " + halfNum + " (напишите пожалуйста + или - ):  ");
+                Console.Write("This number less or equal to " + halfNum + " (write please + or - ):  ");
                 string yesOrNo = Console.ReadLine();
 
                 if (yesOrNo[0] == '+')
@@ -38,7 +39,7 @@ namespace Поиск_числа
                     halfNum = FindNumber(halfNum + 1, until, ((int)(halfNum + until) / 2));
                 else
                 {
-                    Console.WriteLine("+ или - пожалуйста");
+                    Console.WriteLine("+ or - please");
                     halfNum = FindNumber(from, until, halfNum);
                 }
             }
